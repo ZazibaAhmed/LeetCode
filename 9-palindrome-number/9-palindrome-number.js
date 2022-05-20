a852aaf4d64be28806531475e73220adf7676447
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2022-05-20 22:32:05 +0600

    Time: 280 ms (33.92%), Space: 51.8 MB (11.78%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -1,6 +1,5 @@
 var isPalindrome = function(x) {
     const array = x.toString().split('');
     let j = array.length -1;
-    // const reversedArray = [...array].reverse();
     return array.every((elem, index) =>elem===array[j--]);  
 };
