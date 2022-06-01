1fd65b714c8fbca08ec48cf9584d538668956d8d
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2022-06-01 14:01:49 +0600

    Time: 1177 ms (5.02%), Space: 43.6 MB (70.52%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -5,7 +5,7 @@
 var singleNumber = function(nums) {
     for(let i=0; i<nums.length; i++){
         let double = false;
-        if( nums[i]!==null){
+        if(nums[i]!==null){
             for(let j=i+1; j<nums.length; j++){
                 if(nums[i]===nums[j]){
                     double = true;
