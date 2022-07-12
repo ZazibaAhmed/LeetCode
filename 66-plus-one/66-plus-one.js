99c56aa3073b3273805b489b88a97fc3c7b4ec92
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2022-07-12 17:34:00 +0600

    Time: 122 ms (5.69%), Space: 45.1 MB (7.21%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -5,44 +5,31 @@
 var plusOne = function(digits) {
     let reversedArr = digits.reverse();
     let carry = 0;
-    
-    // if(digits.length>1){
-        let plusOne = reversedArr[0]+1;
-        if( plusOne > 9 ){
-            carry = 1;
-            reversedArr[0] = String(plusOne).split('')[1]
-            reversedArr = reversedArr.map(Number)
-            for(let i=1; i<reversedArr.length; i++){
-                if(reversedArr[i]+carry>9){
-                
-                    let sum = reversedArr[i]+carry;
-                    reversedArr[i] = Number(String(sum).split('')[1]);
-                    carry = 1;
-                    console.log(i,reversedArr);
-                }else{
-                    reversedArr[i] = reversedArr[i]+carry;
-                    carry = 0;
-                    return reversedArr.reverse();
-                }
-                console.log(reversedArr);
+    let plusOne = reversedArr[0]+1;
+    if( plusOne > 9 ){
+        carry = 1;
+        reversedArr[0] = String(plusOne).split('')[1]
+        reversedArr = reversedArr.map(Number)
+        for(let i=1; i<reversedArr.length; i++){
+            if(reversedArr[i]+carry>9){
+
+                let sum = reversedArr[i]+carry;
+                reversedArr[i] = Number(String(sum).split('')[1]);
+                carry = 1;
+                console.log(i,reversedArr);
+            }else{
+                reversedArr[i] = reversedArr[i]+carry;
+                carry = 0;
+                return reversedArr.reverse();
             }
-            if(carry===1){
-                reversedArr.push(1);
-            }
-            return reversedArr.reverse();
-        }else{
-            reversedArr[0] = plusOne;
-            return reversedArr.reverse();
+            console.log(reversedArr);
         }
-//     }else{
-//         let sum = digits[0]+1;
-//         digits[0] = digits[0]+1;
-//         if(sum>9){
-//             // const arr
-//             console.log(String(sum).split(''))
-//             return String(sum).split('')
-//         }
-        
-//         return digits
-//     }
+        if(carry===1){
+            reversedArr.push(1);
+        }
+        return reversedArr.reverse();
+    }else{
+        reversedArr[0] = plusOne;
+        return reversedArr.reverse();
+    }
 };
