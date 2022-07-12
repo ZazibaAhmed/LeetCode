47a1fe72e1f410465134f3f7811e4be1bee26448
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2022-07-12 17:45:54 +0600

    Time: 111 ms (13.28%), Space: 45.6 MB (7.21%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -6,9 +6,9 @@ var plusOne = function(digits) {
     let reversedArr = digits.reverse();
     let carry = 0;
     let plusOne = reversedArr[0]+1;
-    if( plusOne > 9 ){
-        carry = 1;
-        reversedArr[0] = String(plusOne).split('')[1]
+    // if( plusOne > 9 ){
+        carry = plusOne > 9 ? 1 : 0;
+        reversedArr[0] = plusOne > 9 ? String(plusOne).split('')[1] :  reversedArr[0] = plusOne;
         reversedArr = reversedArr.map(Number)
         for(let i=1; i<reversedArr.length; i++){
             if(reversedArr[i]+carry>9){
@@ -28,8 +28,8 @@ var plusOne = function(digits) {
             reversedArr.push(1);
         }
         return reversedArr.reverse();
-    }else{
-        reversedArr[0] = plusOne;
-        return reversedArr.reverse();
-    }
+    // }else{
+    //     reversedArr[0] = plusOne;
+    //     return reversedArr.reverse();
+    // }
 };
